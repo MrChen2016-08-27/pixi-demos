fe53abb63ea0ec05d696e0b0f2eb413a91f0ad47
chenjianbin <1909916440@qq.com>
2017-12-18 18:06:30 +0800

    将sprite添加到Round属性中，方便Round中改变属性

--- src/round/Round1.js
+++ src/round/Round1.js
@@ -5,30 +5,34 @@ const loader = addResource('water', 'cat');
 class Round1{
   constructor(app) {
     this.app = app;
+    this.water = null;
+    this.cat = null;
     loader.load((loaderer, resources) => {
       this.initWater(resources);
       this.initCat(resources);
     });
   }
   initWater(resources) {
-    const water = this.getSprite(resources, 'water');
+    this.water = this.getSprite(resources, 'water');
     // 设置位置
-    water.x = this.app.renderer.width / 2;
-    water.y = this.app.renderer.height / 2;
+    this.water.x = this.app.renderer.width / 2;
+    this.water.y = this.app.renderer.height / 2;
     // 设置中心
-    water.anchor.x = 0.5;
-    water.anchor.y = 0.5;
-    this.app.stage.addChild(water);
+    this.water.anchor.x = 0.5;
+    this.water.anchor.y = 0.5;
+    // 添加到舞台
+    this.app.stage.addChild(this.water);
   }
   initCat(resources) {
-    const cat = this.getSprite(resources, 'cat');
+    this.cat = this.getSprite(resources, 'cat');
     // 设置位置
-    cat.x = cat.width;
-    cat.y = this.app.renderer.height - cat.height;
+    this.cat.x = this.cat.width;
+    this.cat.y = this.app.renderer.height - this.cat.height;
     // 设置中心
-    cat.anchor.x = 0.5;
-    cat.anchor.y = 0.5;
-    this.app.stage.addChild(cat);
+    this.cat.anchor.x = 0.5;
+    this.cat.anchor.y = 0.5;
+    // 添加到舞台
+    this.app.stage.addChild(this.cat);
   }
   getSprite(resources, name) {
     const { texture } = resources[name];
