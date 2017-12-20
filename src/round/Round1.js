100bc8af19e191da68a56bcd1ee74eb287e2900b
chenjianbin <1909916440@qq.com>
2017-12-20 18:58:34 +0800

    加载进度监控, 位置方法修改

--- src/round/Round1.js
+++ src/round/Round1.js
@@ -1,4 +1,4 @@
-import addResource from '@/gameModels/addResource';
+import addResource from '@/resources/addResource';
 
 const loader = addResource('water', 'cat');
 
@@ -10,24 +10,30 @@ class Round1{
     loader.load((loaderer, resources) => {
       this.initWater(resources);
       this.initCat(resources);
+      console.log('加载完成');
+    }).on('progress', (loader, res) => {
+      console.log(`加载中...${res.url} ${loader.progress}%`);
     });
   }
   initWater(resources) {
     this.water = this.getSprite(resources, 'water');
+    const x = this.app.renderer.width / 2;
+    const y = this.app.renderer.height / 2;
     // 设置位置
-    this.water.x = this.app.renderer.width / 2;
-    this.water.y = this.app.renderer.height / 2;
+    this.water.position.set(x, y);
     // 设置中心
     this.water.anchor.x = 0.5;
     this.water.anchor.y = 0.5;
+
     // 添加到舞台
     this.app.stage.addChild(this.water);
   }
   initCat(resources) {
     this.cat = this.getSprite(resources, 'cat');
+    const x = this.cat.width;
+    const y = this.app.renderer.height - this.cat.height;
     // 设置位置
-    this.cat.x = this.cat.width;
-    this.cat.y = this.app.renderer.height - this.cat.height;
+    this.cat.position.set(x, y);
     // 设置中心
     this.cat.anchor.x = 0.5;
     this.cat.anchor.y = 0.5;
