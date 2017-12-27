63ea15822c4306cd9318eb23ba6746cecc1954fe
chenjianbin <1909916440@qq.com>
2017-12-27 16:59:02 +0800

    修改 Controller 中的 rangeMove 方法及参数， 使用 sprite 的vx, vy来表示速度与方向

--- src/control/Controller.js
+++ src/control/Controller.js
@@ -19,11 +19,11 @@ class Controller{
    * @param  {Sprite对象} sprite   [用于改变对象的移动属性]
    * @param  {Object} range    [限制移动范围，startX 开始移动x坐标, startY 开始移动Y坐标, endX 结束移动x坐标, endY 结束移动y坐标]
    * @param  {Object} distance [移动距离, { x,y }, 应该放在 app.ticker.add 外层]
-   * @return {Object}          [返回被改变后的移动距离对象]
+   * @return {Object}          [返回被移动后的Sprite对象]
    */
   rangeMove(sprite, range, distance) {
-    let distanceX = distance.x;
-    let distanceY = distance.y;
+    let distanceX = sprite.vx || 1;
+    let distanceY = sprite.vy || 1;
     const xStartValidate = sprite.x < range.startX && distanceX < 0;
     const xEndValidate = sprite.x > range.endX && distanceX > 0;
     const yStartValidate = sprite.y < range.startY && distanceY < 0;
@@ -36,10 +36,9 @@ class Controller{
     }
     sprite.x += distanceX;
     sprite.y += distanceY;
-    return {
-      x: distanceX,
-      y: distanceY
-    };
+    sprite.vx = distanceX;
+    sprite.vy = distanceY;
+    return sprite;
   }
 }
 
--- src/round/Round1.js
+++ src/round/Round1.js
@@ -4,9 +4,8 @@ import Round from '@/round/Round';
 
 
 class Round1 extends Round{
-  constructor(app) {
+  constructor() {
     super('round1');
-    game = app;
     // 关卡总共水资源集合
     this.waters = [];
     this.cat = null;
@@ -22,19 +21,16 @@ class Round1 extends Round{
   }
 
   initWater(waterTexture) {
-    const self = this;
-    const x = self.app.renderer.width / 2;
-    const y = self.app.renderer.height / 2;
+    const x = game.renderer.width / 2;
+    const y = game.renderer.height / 2;
     const waterControl = new WaterControl();
     const waterSprite = new PIXI.Sprite(waterTexture);
     waterSprite.position.set(x, y);
     waterSprite.pivot.set(0.5, 0.5);
     // 添加到舞台
     game.stage.addChild(waterSprite);
-    let distance = {
-      x: Math.round(Math.random() * 3 + 0.5),
-      y: Math.round(Math.random() * 3 + 0.5)
-    };
+    waterSprite.vx = Math.round(Math.random() * 3 + 0.5);
+    waterSprite.vy = Math.round(Math.random() * 3 + 0.5);
     const range = {
       startX: this.cat.x,
       startY: 0,
@@ -43,14 +39,13 @@ class Round1 extends Round{
     };
     this.waters.push(waterSprite);
     game.ticker.add(delta => {
-      distance = waterControl.rangeMove(waterSprite, range, distance);
+      waterControl.rangeMove(waterSprite, range);
     });
   }
   initCat(catTexture) {
-    const self = this;
     const sprite = new PIXI.Sprite(catTexture);
     const x = sprite.width;
-    const y = self.app.renderer.height - sprite.height;
+    const y = game.renderer.height - sprite.height;
     sprite.position.set(x, y);
     sprite.pivot.set(0.5, 0.5);
     this.cat = sprite;
